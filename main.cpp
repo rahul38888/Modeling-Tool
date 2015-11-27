35f83356f6f5d51aea44f10d25da8904a1e78550
Rahul <rahul38888@gmail.com>
2015-11-28 00:54:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,7 @@
-#include <windows.h>
-#include <stdio.h>
-//#include <GL/glew.h>
-#include <GL/glui.h>
-//#include <GL/glut.h>
-//#include <GL/gl.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
-#include <string.h>
-
-//#include <cstring>
-#include <iostream>
-#include <sstream>
+#include "includedlibs.h"
 
 float scale_factor=100;
 float cam_scale=100;
-
-#include "dat_stuct.h"
-#include "texture.h"
-#include "splines.h"
-#include "object.h"
-#include "minecraft_mode.h"
-#include "object_draw.h"
-#include "create_object.h"
-#include "default_objects.h"
-#include "light.h"
-#include "text_info.h"
-#include "control_info.h"
-#include "gravity_physics.h"
-#include "save_file.h"
-#include "load_file.h"
-#include "shaderReader.h"
-
-#include "shaderLoad.h"
 
 #define PI 3.14159
 
