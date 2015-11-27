d714327bfad3be96fe74a66005794d466f88dc24
Rahul <rahul38888@gmail.com>
2015-11-28 00:53:02 +0530

    Create includedlibs.h

---
+++ includedlibs.h
@@ -0,0 +1,32 @@
+#include <windows.h>
+#include <stdio.h>
+//#include <GL/glew.h>
+#include <GL/glui.h>
+//#include <GL/glut.h>
+//#include <GL/gl.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+//#include <cstring>
+#include <iostream>
+#include <sstream>
+
+#include "dat_stuct.h"
+#include "texture.h"
+#include "splines.h"
+#include "object.h"
+#include "minecraft_mode.h"
+#include "object_draw.h"
+#include "create_object.h"
+#include "default_objects.h"
+#include "light.h"
+#include "text_info.h"
+#include "control_info.h"
+#include "gravity_physics.h"
+#include "save_file.h"
+#include "load_file.h"
+#include "shaderReader.h"
+
+#include "shaderLoad.h"
